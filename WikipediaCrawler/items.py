620117d52ae489de0a01aa00f8b6040c06e861bc
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2017-01-26 20:44:56 +0330

    implement spider items

--- WikipediaCrawler/items.py
+++ WikipediaCrawler/items.py
@@ -8,7 +8,8 @@
 import scrapy
 
 
-class WikipediacrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class WikipediaPage(scrapy.Item):
+    title = scrapy.Field()
+    brief = scrapy.Field()
+    text = scrapy.Field()
     pass
--- WikipediaCrawler/spiders/wikipedia-spider.py
+++ WikipediaCrawler/spiders/wikipedia-spider.py
@@ -1,16 +1,25 @@
 import scrapy
+from bs4 import BeautifulSoup
 
 class WikipediaSpider(scrapy.Spider):
     name = 'wikipedia'
 
     number_of_pages = 1000
     out_degree = 10
+    allowed_domains = ['fa.wikipedia.org']
     start_urls = [
         'https://fa.wikipedia.org/wiki/%D8%B3%D8%B9%D8%AF%DB%8C',
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        with open(page, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % page)
+        content = BeautifulSoup(response)
+        yield {
+            'title': content.h1.string,
+            'brief': content.p.string.get_text(),
+            'text': content.find(id="toc").next_siblings.get_text()
+        }
+
+        for link in content.find_all('a'):
+            next_page = link.get("href")
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
